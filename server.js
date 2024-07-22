d2afc7c21830e76e1b85c91141b9f1e04a19f23b
HENNING KÖNITZ <koenitz92@gmx.de>
2024-07-22 16:31:40 +0200

    add cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const { sql } = require("@vercel/postgres");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({ msg: "Hello from the Notes App - This is the demo" });
